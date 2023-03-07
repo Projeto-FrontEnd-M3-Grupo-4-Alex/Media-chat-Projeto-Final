3e04087278fcbad5a279a35b4a0a957fef645532
Bruna S <bruuhbruna78@gmail.com>
2023-03-07 12:28:47 -0300

    feat: create services/api,  usercontext,  postsContext

---
+++ media-chat/src/providers/PostsContext/@types.ts
@@ -0,0 +1,37 @@
+export interface IPost {
+  userId: number;
+  thumbnail: string;
+  title: string;
+  content: string;
+  tags?: [];
+  where: string;
+  rating?: string;
+  id: number;
+}
+
+export interface IPostFormValues {
+  userId: number;
+  thumbnail: string;
+  title: string;
+  content: string;
+  tags?: [];
+  where: string;
+  rating?: string;
+}
+
+export interface IResponsePost {
+  post: IPost;
+}
+
+export interface IResponsePosts {
+  posts: IPost[];
+}
+
+export interface IPostsContext {
+  post: IPost | null;
+  posts: IPost[] | null;
+  PostsRead: () => Promise<void>;
+  PostCreate: (formData: IPostFormValues) => Promise<void>;
+  PostUpdate: (postId: number, formData: IPostFormValues) => Promise<void>;
+  PostDelete: (postId: number) => Promise<void>;
+}
