de87bae04f133c5df071aea636a427900d7140f9
Bruna S <bruuhbruna78@gmail.com>
2023-03-10 08:30:31 -0300

    feat: resolver conflit

--- media-chat/src/pages/Login/index.tsx
+++ media-chat/src/pages/Login/index.tsx
@@ -1,8 +1,4 @@
-
-import { LoginForm } from "../../components/Form/LoginForm"
-import { Link } from "react-router-dom"
-import logo from "../../assets/logo.svg"
-import { FormStyle } from "../../styles/FormStyle"
+import { FormStyle } from "../../styles/FormStyle";
 import { LoginForm } from "../../components/Form/LoginForm";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
@@ -14,17 +10,17 @@ export function LoginPage() {
   const { loading } = useContext(UserContext);
   return (
     <FormStyle>
-    {loading ? (
+      {loading ? (
         <Loading />
       ) : (
-      <div className="loginContainer">
-        <img src={logo} alt="Logo Media Chat" />
-        <h2>Login</h2>
-        <LoginForm />
-        <p>Ainda não cadastrado ?</p>
-        <Link to={"/register"}>Ir para a página de cadastro</Link>
-      </div>
-      )
+        <div className="loginContainer">
+          <img src={logo} alt="Logo Media Chat" />
+          <h2>Login</h2>
+          <LoginForm />
+          <p>Ainda não cadastrado ?</p>
+          <Link to={"/register"}>Ir para a página de cadastro</Link>
+        </div>
+      )}
     </FormStyle>
-  )
+  );
 }
